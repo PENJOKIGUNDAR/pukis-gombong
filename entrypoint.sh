5d64c9aef942820e840fa9ffc9cf29157881766f
Ryvins <asrofiddin@gmail.com>
2025-08-05 08:50:57 +0700

    deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,19 @@
 # Install composer dependencies
 composer install --no-dev --optimize-autoloader
 
-# Run migration langsung
+# Jalankan migration
 echo "🚀 Jalankan migration..."
 php artisan migrate --force --no-interaction
+
+# Jalankan seeder
+echo "🌱 Jalankan seeder..."
+php artisan db:seed --force
 
 # (Optional) cache config & route kalau production
 echo "⚙️ Optimize Laravel..."
 php artisan config:cache
 php artisan route:cache
 
-# Start PHP-FPM
+# Start PHP built-in server
 echo "✅ Siap terima request!"
 php artisan serve --host=0.0.0.0 --port=8080
